609d060a26de9ff26fd27f95b467f83c4c6fef3b
Cameron <woodward.cf@gmail.com>
2017-04-26 20:39:07 -0700

    Added sketch functionality

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   for(var i=0; i<16; i++) {
     for(var j=0; j<16; j++) {
-      var unit = $('<div></div>');
-      unit.appendTo('.container');
+      var unit = $('<div class="blox"></div>');
+      unit.appendTo('#container');
     }
   }
+
+  $('.blox').on('mouseenter', function() {
+    $(this).addClass('highlight');
+  });
 });
