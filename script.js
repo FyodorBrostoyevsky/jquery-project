227624bbaa8127ca47066bfa926499d96cf2d39f
Cameron <woodward.cf@gmail.com>
2017-05-08 15:04:48 -0700

    Fixed triangle grid issue

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
   for(var i=0; i<16; i++) {
-    var row = $("<tr></tr>");
+    var row = $("<tr class='row'></tr>");
     row.appendTo('#canvas');
+  };
+
+  for(var j=0; j<16; j++) {
     var column = $("<td class='blox'></td>");
     column.appendTo('tr');
+
   };
 
   $('.blox').on('mouseenter', function() {
     $(this).addClass('highlight');
   });
 });
+
+
+// Should my table cells have divs within them?
+// Must remove gaps between data rows
+// How to scale dynamically?
