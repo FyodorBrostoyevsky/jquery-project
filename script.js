b90ec469afeb85f387c5cdf1cb904fb93300782d
Cameron <woodward.cf@gmail.com>
2017-05-10 16:54:23 -0700

    Resize and reset functionality added

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
-  for(var i=0; i<16; i++) {
-    var row = $("<tr class='row'></tr>");
-    row.appendTo('#canvas');
+  // Create a table with variable dimensions
+  function sketchup(x) {
+    for(var i=0; i<x; i++) {
+      var row = $("<tr class='row'></tr>");
+      row.appendTo('#canvas');
+    };
+
+    for(var j=0; j<x; j++) {
+      var column = $("<td class='blox'></td>");
+      column.appendTo('tr');
+    };
   };
 
-  for(var j=0; j<16; j++) {
-    var column = $("<td class='blox'></td>");
-    column.appendTo('tr');
 
-  };
+sketchup(70);
+
+  $('#resize').submit(function() {
+    var size = $('form').val();
+    sketchup(size);
+  })
 
   $('.blox').on('mouseenter', function() {
     $(this).addClass('highlight');
   });
+  $('#reset').on('click', function() {
+    $('.blox').removeClass('highlight');
+  });
 });
+
 
 
 // Should my table cells have divs within them?
